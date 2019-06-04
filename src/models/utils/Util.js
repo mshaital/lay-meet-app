689de45e504312db20cabdf984780054513a023e
shaital <neel_shaital@foxmail.com>
2019-06-04 17:42:41 +0800

    完善图片上传功能

--- src/models/utils/Util.js
+++ src/models/utils/Util.js
@@ -37,48 +37,54 @@ const Util = {
     })
   },
   /**
-   * 压缩图片
-   * @description  * img    原始图片
-   * @description width   压缩后的宽度
-   * @description height  压缩后的高度
-   * @description ratio   压缩比率
+   * @description   压缩图片
+   * @param  * img    原始图片
+   * @param width   压缩后的宽度
+   * @param height  压缩后的高度
+   * @param ratio   压缩比率
    * @returns {String}
    */
   dealImage (img, width, height, ratio) {
-    let canvas, ctx, img64
-    canvas = document.createElement('canvas')
-    canvas.width = width
-    canvas.height = height
-    ctx = canvas.getContext('2d')
-    ctx.drawImage(img, 0, 0, width, height)
-    img64 = canvas.toDataURL('img/jpeg', ratio)
-    function dataURLtoBlob (dataurl) {
-      let arr = dataurl.split(','), mime = arr[0].match(/:(.*?);/)[1],
-        bstr = atob(arr[1]), n = bstr.length, u8arr = new Uint8Array(n);
-      while (n--) {
-        u8arr[n] = bstr.charCodeAt(n);
-      }
-      return new Blob([u8arr], {type: mime});
-    }
+    return new Promise(function (resolve, reject) {
+      let canvas, ctx
+      canvas = document.createElement('canvas')
+      canvas.width = width
+      canvas.height = height
+      ctx = canvas.getContext('2d')
+      ctx.drawImage(img, 0, 0, width, height)
+      // img64 = canvas.toDataURL('img/jpeg', ratio)
+      canvas.toBlob(blob => resolve(blob), "image/jpeg", ratio)
+    });
 
-    return dataURLtoBlob(img64)
   },
-  onUpload (file) {
-    let _this = this
-    let image = new Image()
-    image.src = URL.createObjectURL(file.file)
-    image.onload = () => {
-      let imgblob = _this.dealImage(image, 70, 70, 0.5)
-      // 发送请求
+  getImgUploadToken () {
+    return new Promise(function (resolve, reject) {
       coopService.getImgUploadToken({}).then(res => {
-        console.log(res)
-        console.log(imgblob)
-        _this.uploadImg(res, imgblob)
+        if (!res) return
+        resolve(res);
       })
-
-    }
+    });
   },
-  uploadImg(token, file) {
+  newImage (file) {
+    return new Promise(function (resolve, reject) {
+      let image = new Image()
+      image.src = URL.createObjectURL(file.file)
+      image.onload = () => {
+        resolve(image);
+      }
+    });
+  },
+  asyncUploadImg: async function (file, w, h, ratio, callBack) {
+    let _this = this
+    const image = await _this.newImage(file);
+    const imgBlob = await _this.dealImage(image, w, h, ratio);
+    const token = await _this.getImgUploadToken();
+    _this.qiniuUploadImg(token, imgBlob, callBack)
+  },
+  qiniuUploadImg(token, file, callBack) {
+    // console.log(token)
+    // console.log(file)
+
     const key = file.name  // 上传后文件资源名以设置的 key 为主，如果 key 为 null 或者 undefined，则文件资源名会以 hash 值作为资源名。
     let config = {
       useCdnDomain: true,   //表示是否使用 cdn 加速域名，为布尔值，true 表示使用，默认为 false。
@@ -102,14 +108,7 @@ const Util = {
       },
       complete: (res) => {
         console.log(res.hash)
-        // coopService.upload(params).then(res => {
-        //   if (res !== 'SUCCESS') return
-        //   let userInfo = store.state.userInfo
-        //   userInfo.head_img = img64
-        //   store.commit('SET_USER_INFO', userInfo)
-        //   Toast('上传成功')
-        //   return img64
-        // })
+        callBack(res)
       }
     })
   },
--- src/views/user/BaseSetting.vue
+++ src/views/user/BaseSetting.vue
@@ -3,7 +3,7 @@
     <nav-title title="个人资料" rightText="保存" @right-click="userModify"></nav-title>
     <div class="mt-5">
       <div class="head-img mx-auto d-block">
-        <img :src="userInfo.head_img">
+        <!--<img :src="userInfo.head_img">-->
         <van-uploader class="upload-img" :after-read="onRead">
           <van-icon name="photograph" color="#1989fa"/>
         </van-uploader>
@@ -20,8 +20,8 @@
     <div class="bg-white p-3 font-14 border-bottom d-flex">
       <span class="mr-4">性别</span>
       <van-radio-group v-model="userInfo.sex" class="d-flex">
-        <van-radio name="m" class="mr-4"  checked-color="#07c160">先生</van-radio>
-        <van-radio name="f"  checked-color="#07c160">小姐</van-radio>
+        <van-radio name="m" class="mr-4" checked-color="#07c160">先生</van-radio>
+        <van-radio name="f" checked-color="#07c160">小姐</van-radio>
       </van-radio-group>
     </div>
 
@@ -38,7 +38,7 @@
   import newValidator from '~utils/validator'
   import Util from '~utils/Util'
   import coopService from '~modules/coopService'
-  import { Toast } from 'vant'
+  import {Toast} from 'vant'
   import * as qiniu from 'qiniu-js'
 
   export default {
@@ -51,50 +51,19 @@
       }
     },
     methods: {
-      uploadImg(token, file) {
-        const key = file.name  // 上传后文件资源名以设置的 key 为主，如果 key 为 null 或者 undefined，则文件资源名会以 hash 值作为资源名。
-        let config = {
-          useCdnDomain: true,   //表示是否使用 cdn 加速域名，为布尔值，true 表示使用，默认为 false。
-          region: qiniu.region.z0     // 根据具体提示修改上传地区,当为 null 或 undefined 时，自动分析上传域名区域
-        };
-
-        let putExtra = {
-          fname: "",  //文件原文件名
-          params: {}, //用来放置自定义变量
-          mimeType: null  //用来限制上传文件类型，为 null 时表示不对文件类型限制；限制类型放到数组里： ["image/png", "image/jpeg", "image/gif"]
-        };
-        let observable = qiniu.upload(file, key, token, putExtra, config);
-        observable.subscribe({
-          next: (res) => {
-            let total = res.total;
-             console.log(res)
-             console.log("进度：" + parseInt(total.percent) + "% ")
-          },
-          error: (err) => {
-            console.log(err)
-          },
-          complete: (res) => {
-            console.log(res.hash)
-          }
-        })
-      },
       onRead (file) {
-        this.userInfo.head_img = Util.Util.onUpload(file)
-        let params = {}
-        let _this = this
-//        coopService.getToken(params).then(res => {
-//         console.log(res)
-//          _this.uploadImg(res,file.file)
-//        })
+        Util.Util.asyncUploadImg(file, 70, 70, .5, res => {
+          console.log(res)
+        })
       },
 
       userModify () {
         let _this = this
         let params = {
-          user_id:this.userInfo.user_id,
-          nick:this.userInfo.nick,
-          sex:this.userInfo.sex,
-          declaration:this.userInfo.declaration,
+          user_id: this.userInfo.user_id,
+          nick: this.userInfo.nick,
+          sex: this.userInfo.sex,
+          declaration: this.userInfo.declaration,
         }
 
         let checkList = [
@@ -120,29 +89,33 @@
 <style lang="scss" scoped>
 
   @import "../../assets/css/currency";
-  input{
+
+  input {
     border: none;
     outline: none;
   }
-  textarea{
+
+  textarea {
     width: 100%;
     border: none;
     outline: none;
     resize: none;
   }
-  .content{
+
+  .content {
     height: 100vh;
   }
-  .head-img{
+
+  .head-img {
     width: 70px;
     height: 110px;
     position: relative;
-    .upload-img{
+    .upload-img {
       position: absolute;
       bottom: 10px;
       right: 5px;
     }
-    img{
+    img {
       margin: 20px 0;
       width: 100%;
       border-radius: 35px;
